94ef30253d31445294594db4fe824b0d8edb6418
Clayton Fan <cfan@benning.net>
2018-05-10 03:26:06 -0400

    Add files via upload

--- eco.c
+++ eco.c
@@ -10,27 +10,26 @@
 #include "util.h"
 #include "wm.h"
 #include "fit.h"
-
-int  midi_soundcheck( T_PARAM * );
+//
+int  midi_soundcheck(                 T_PARAM * );
+int  write_song(          T_WORLD  *, T_PARAM * );
 //
 void genesis(             T_WORLD  *, T_PARAM * );
 void apocalypse(          T_WORLD  *, T_PARAM * );
-void diverge(             T_WORLD  *, T_PARAM * );
-void organize(            T_WORLD  *, T_PARAM * );
-int  write_song(          T_WORLD  *, T_PARAM * );
-//
-void  adjust_ecosystem(   T_WORLD  *, T_PARAM * );
-void enhance_ecosystem(   T_WORLD  *, T_PARAM * );
-void express_genes(       T_SPECIE *, T_PARAM * );
-//
-void display_final(       T_WORLD  *,       T_PARAM *p );
-void display_data(  int,  T_SPECIE *, bool, T_PARAM *p );
-//
 // Genetic Evolution
-//
 int  select_parent( int,           T_LIFE *,             char, T_PARAM * );
 void crossover(     int, T_LIFE *, T_LIFE *, T_PARENTS *, int, T_PARAM * );
 void mutate(        int,           T_LIFE *,              int, T_PARAM * );
+// Epigenetics
+void adjust_ecosystem(    T_WORLD  *, T_PARAM * );
+void enhance_ecosystem(   T_WORLD  *, T_PARAM * );
+int  express_genes(       T_SPECIE *, T_PARAM * );//
+// Self Organization
+int  diverge(             T_WORLD  *, T_PARAM * );
+int  organize(            T_WORLD  *, T_PARAM * );
+//
+void display_final(       T_WORLD  *,       T_PARAM *p );
+void display_data(  int,  T_SPECIE *, bool, T_PARAM *p );
 //
 static T_WORLD    world;
 //
@@ -38,11 +37,11 @@ static T_WORLD    world;
 //
 int eco( T_PARAM *p )
 {
-  int i, j, status = 0;
+  int i, j, k;
   T_PARENTS  parents;
   T_SPECIE  *specie0, *specie1, *genswap;
 
-  if( p->trace >= 0 ) write_out( "Initiating Ecosystem. . .\n");
+  if( p->trace >= 0 ) Print( "Initiating Ecosystem. . .\n");
   //
   // Create world
   // Define environment, and
@@ -61,18 +60,24 @@ int eco( T_PARAM *p )
     // Express genes of all organisms in one generation
     // Compute fitness for all species of the current generation.
     //
-    express_genes( world.gen0, p );
+    if( express_genes( world.gen0, p ) < NORMAL ) break;
+	//exit(1);     // debug
+	Print("Compute Fitness\n");
     compute_fitness( &world,   p );
-    if( fit_to_exit(           p )) break;
+	Print("Fit to exit\n");
+    if( fit_to_exit(           p ) ) break;
     //
     // Epigenetic Interventions
     //
+	Print("Adj Eco...");
     adjust_ecosystem( &world, p );
+	Print("...END\n");
     //
     // Evolve the population of species.
     // Some species may diverge after initial evolution.
     //
     for( i = 0; i < p->nspecies; i++) {
+		//Print("Specie %d'n", i);
      
       if( p->species[i].active ) {
 
@@ -106,44 +111,48 @@ int eco( T_PARAM *p )
     // 
     // Diverge new species  - organize before diverge?
     //
-    diverge( &world, p );
+    if( diverge( &world, p ) < NORMAL ) break;
     //
     // Organize Community
-    // 
-    organize( &world, p );
+    //
+    if( organize( &world, p ) < NORMAL ) break;
     //
     // Swap new with old generation of organisms for all species.
     //
     genswap    = world.gen1;
     world.gen1 = world.gen0;
     world.gen0 = genswap;
-    for( i = 0; i < p->nspecies; i++ ) p->species[i].sp = &(world.gen0[i]);
+    for( i = 0; i < p->nspecies; i++ ) {
+      p->species[i].sp = &(world.gen0[i]);
+    }
   }                              // For all generations
-  //
-  // End of ecosystem evolution.
-  //   - Get final fittest information.
-  //   - Write song to file in MIDI format.
-  // Note that after the swap, gen0 is the current generation.
-  //
-  if( p->igens >= p->ngens ) {
-    express_genes( world.gen0, p );
-    compute_fitness ( &world, p );
-  }
-  enhance_ecosystem( &world, p );
-  for( i = 0; i < p->nspecies; i++) {
-    if( p->species[i].active ) {
-      display_data(  i, &(world.gen0[i]), TRUE, p );
+  if( p->status >= NORMAL ) {
+    //
+    // End of ecosystem evolution.
+    //   - Get final fittest information.
+    //   - Write song to file in MIDI format.
+    // Note that after the swap, gen0 is the current generation.
+    //
+    if (p->igens >= p->ngens) {
+        if (express_genes(world.gen0, p) < NORMAL) goto end_of_eco;
+        compute_fitness(&world, p);
+    }
+    enhance_ecosystem(&world, p);
+    for( k = 0; k < p->nspecies; k++ ) {
+      if( p->species[k].active ) {
+        display_data(k, &(world.gen0[k]), TRUE, p);
+      }
+    }
+    if( write_song(&world, p) >= NORMAL ) {
+      display_final(&world, p);
+      Print("Completed.\n");
+    } else {
+      Print("MIDI file not written (%d).\n", p->status );
     }
   }
-  if( status = ( write_song( &world, p ) ) ) {
-      display_final( &world, p );
-      Print( "Completed.\n" );
-  }
-  else {
-      Print( "MIDI file not written.\n");
-  }
+  end_of_eco:
   // Destroy world -- population and environment
   apocalypse( &world, p );
-  return( status );
+  Print("End of Apo\n");
+  return( p->status );
 }
-
